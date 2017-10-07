b742720d8c1dc27c281ae303e4446e86c2668d8f
Andrews Frempong <frempongandrews@yahoo.com>
2017-10-07 03:55:18 +0100

    addition operator implemented

--- 02-dailyProjects-Calculator/02-dailyProjects-Calculator/ViewController.swift
+++ 02-dailyProjects-Calculator/02-dailyProjects-Calculator/ViewController.swift
@@ -9,6 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    var buttonsArr: [UIButton] = []
+    var buttonTitles = ["None", "Add", "Subtract", "Multiply", "Divide", "Compute"]
+    var leftAnchor:NSLayoutXAxisAnchor?
+    
+    
+    //<!-- operator label -->
+    
+    let operatorLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .red
+        label.text = "+"
+        label.textAlignment = .center
+        
+        label.layer.masksToBounds = true //for cornerRadius
+        label.layer.cornerRadius = 5
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    //<!-- first number -->
+    
+    let firstNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "X"
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .lightGray
+        textField.textAlignment = .center
+        textField.keyboardType = .numberPad
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    //<!-- second number -->
+    
+    let secondNumberTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Y"
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = .lightGray
+        textField.keyboardType = .numberPad
+        textField.textAlignment = .center
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    
+    //<!-- Result label-->
+    let resultLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .lightGray
+        label.text = "Result"
+        label.textAlignment = .right
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    //<!-- Operators container-->
+    
+    
+    let operatorsContainerView: UIView = {
+        let v = UIView()
+        v.backgroundColor = .green
+        v.layer.borderColor = UIColor.red.cgColor
+        v.layer.borderWidth = 1
+        
+        v.translatesAutoresizingMaskIntoConstraints = false
+        return v
+    }()
+    
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,7 +95,90 @@ class ViewController: UIViewController {
         setupLayout ()
     }
     
+    
+    //<!-- Main operations-->
+    
+    func handleNone () {
+        operatorLabel.text = ""
+        print("handling none")
+    }
+    
+    func handleAdd () {
+        operatorLabel.text = "+"
+    }
+    
+    func handleSubtract () {
+        operatorLabel.text = "-"
+    }
+    
+    func handleMultiply () {
+        operatorLabel.text = "*"
+    }
+    
+    func handleDivide() {
+        operatorLabel.text = "/"
+    }
+    
+    
+    //TODO LOGIC
+    
+    func handleCompute () {
+        
+        var calculationIsReady = false
+        var firstNumberIsReady = false
+        var secondNumberIsReady = false
+        var operatorIsReady = false
+        if let firstNum = firstNumberTextField.text {
+            if firstNum == "" {
+                print("Please insert first number")
+            } else {
+                firstNumberIsReady = true
+            }
+        }
+        
+        if let secondNum = secondNumberTextField.text {
+            if secondNum == "" {
+                print("Please insert second number")
+            } else {
+                secondNumberIsReady = true
+            }
+        }
+        
+        if let op = operatorLabel.text {
+            if op == "" {
+                print("Please operator")
+            } else {
+                operatorIsReady = true
+            }
+        }
+        
+        
+        //ready for calculation
+        if firstNumberIsReady && secondNumberIsReady && operatorIsReady {
+            //print("yay!!")
+            
+            //adding
+            if operatorLabel.text == "+" {
+                
+                if let firstNum = firstNumberTextField.text, let secondNum = secondNumberTextField.text {
+                    let result = Int(firstNum)! + Int(secondNum)!
+                    resultLabel.text = String(describing: result)
+                }
+                
+                
+            }//<!-- end of nigeria
+        }
+        
+        
+        
+    }
+    
+    
+    var functionsArr = [handleNone, handleAdd, handleSubtract, handleMultiply, handleDivide, handleCompute]
+
+    
     func setupLayout () {
+        
         
         
         //<!-- container -->
@@ -42,18 +203,7 @@ class ViewController: UIViewController {
         containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -5).isActive = true
         
         
-        //<!-- first number -->
-        
-        let firstNumberTextField: UITextField = {
-           let textField = UITextField()
-            textField.placeholder = "X"
-            textField.borderStyle = .roundedRect
-            textField.backgroundColor = .lightGray
-            textField.textAlignment = .center
-            textField.keyboardType = .numberPad
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            return textField
-        }()
+
         
         containerView.addSubview(firstNumberTextField)
         
@@ -65,21 +215,10 @@ class ViewController: UIViewController {
         firstNumberTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
         
+        
+        
+        
         //<!-- Operator Label-->
-        
-        
-        let operatorLabel: UILabel = {
-            let label = UILabel()
-            label.backgroundColor = .red
-            label.text = "+"
-            label.textAlignment = .center
-            
-            label.layer.masksToBounds = true //for cornerRadius
-            label.layer.cornerRadius = 5
-            
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
         
         containerView.addSubview(operatorLabel)
         
@@ -92,19 +231,7 @@ class ViewController: UIViewController {
         
         
         
-        //<!-- second number -->
-        
-        let secondNumberTextField: UITextField = {
-            let textField = UITextField()
-            textField.placeholder = "Y"
-            textField.borderStyle = .roundedRect
-            textField.backgroundColor = .lightGray
-            textField.keyboardType = .numberPad
-            textField.textAlignment = .center
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            return textField
-        }()
-        
+       //<!-- Second number -->
         containerView.addSubview(secondNumberTextField)
         
         //constraints
@@ -114,16 +241,9 @@ class ViewController: UIViewController {
         secondNumberTextField.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.25).isActive = true
         secondNumberTextField.heightAnchor.constraint(equalToConstant: 30).isActive = true
         
-        //<!-- Result label-->
-        let resultLabel: UILabel = {
-            let label = UILabel()
-            label.backgroundColor = .lightGray
-            label.text = "Result"
-            label.textAlignment = .right
-            label.isUserInteractionEnabled = false
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
+        
+        
+        //<!-- Result label -->
         
         containerView.addSubview(resultLabel)
         
@@ -137,18 +257,8 @@ class ViewController: UIViewController {
         
         
         
-        //<!-- Operators container-->
-        
-
-        let operatorsContainerView: UIView = {
-            let v = UIView()
-            v.backgroundColor = .green
-            v.layer.borderColor = UIColor.red.cgColor
-            v.layer.borderWidth = 1
-            
-            v.translatesAutoresizingMaskIntoConstraints = false
-            return v
-        }()
+   
+        //<!--Operators container -->
         
         //add container to screen
         
@@ -162,11 +272,13 @@ class ViewController: UIViewController {
         operatorsContainerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
         
+        
+        
+        
+        
         //<!-- Creating 4 buttons with for loop-->
         
-        var buttonsArr: [UIButton] = []
-        var buttonTitles = ["None", "Add", "Subtract", "Multiply", "Divide", "Compute"]
-        var leftAnchor:NSLayoutXAxisAnchor?
+        
         //print(type(of: leftAnchor))
         for i in 0...5 {
             let button = UIButton()
@@ -210,6 +322,9 @@ class ViewController: UIViewController {
                 button.setTitle("Compute", for: .normal)
                 button.backgroundColor = .white
                 button.setTitleColor(.red, for: .normal)
+                buttonsArr.append(button)
+            
+                
                 //add to view
                 
                 containerView.addSubview(button)
@@ -226,18 +341,19 @@ class ViewController: UIViewController {
             
             
             
-            
-            
-            
-            
-            
         } //<!-- End Creating 4 buttons with for loop-->
-
         
         
         
-     
+        buttonsArr[0].addTarget(self, action: #selector(handleNone), for: .touchUpInside)
+        buttonsArr[1].addTarget(self, action: #selector(handleAdd), for: .touchUpInside)
+        buttonsArr[2].addTarget(self, action: #selector(handleSubtract), for: .touchUpInside)
+        buttonsArr[3].addTarget(self, action: #selector(handleMultiply), for: .touchUpInside)
+        buttonsArr[4].addTarget(self, action: #selector(handleDivide), for: .touchUpInside)
+        buttonsArr[5].addTarget(self, action: #selector(handleCompute), for: .touchUpInside)
         
+       
+       
         
         
     }//<!-- End of setupLayout -->
